d7cb7b3f28f358f00e5adb9f6859deb017931c96
nm141 <135771596+nm141@users.noreply.github.com>
2023-09-14 16:06:16 +1000

    Update dec-agent.sh

---
+++ dec-agent.sh
@@ -0,0 +1,3 @@
+https://prod.setup.itsupport247.net/darwin/DPMA/64/Sydney%20(ASEC3307%20DEC%20PR)_macOS_ITSPlatform_TKN84d1fee7-d15e-49cd-a8ff-9c0c7cdc0713/PKG/setup
+
+84d1fee7-d15e-49cd-a8ff-9c0c7cdc0713
